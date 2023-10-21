a63b20e4e5658e0bd20d9e617c8080009f45be87
Oscar Roblez <speculatetoomuch@outlook.com>
2023-10-22 01:37:22 +0800

    first commit

---
+++ src/components/PrimaryFeatures.jsx
@@ -0,0 +1,151 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { Tab } from '@headlessui/react'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import screenshotExpenses from '@/images/screenshots/expenses.jpg'
+import screenshotPayroll from '@/images/screenshots/payroll.jpg'
+import distribution from '@/images/screenshots/distribution.jpg'
+
+const features = [
+  {
+    title: 'Asset Protection',
+    description:
+      "To fully embrace one's intrinsic value, it's essential to link every opportunity in life with significance and potential.",
+    image: screenshotPayroll,
+  },
+  {
+    title: 'Asset Accumulation',
+    description:
+      "Genuine prosperity or riches have a lasting impact, stretching beyond an individual's lifetime to influence countless future generations.",
+    image: screenshotExpenses,
+  },
+  {
+    title: 'Asset Distribution',
+    description:
+      "Concentrate on expanding one's financial capabilities to create optimal opportunities for a fulfilling life",
+    image: distribution,
+  },
+]
+
+export function PrimaryFeatures() {
+  let [tabOrientation, setTabOrientation] = useState('horizontal')
+
+  useEffect(() => {
+    let lgMediaQuery = window.matchMedia('(min-width: 1024px)')
+
+    function onMediaQueryChange({ matches }) {
+      setTabOrientation(matches ? 'vertical' : 'horizontal')
+    }
+
+    onMediaQueryChange(lgMediaQuery)
+    lgMediaQuery.addEventListener('change', onMediaQueryChange)
+
+    return () => {
+      lgMediaQuery.removeEventListener('change', onMediaQueryChange)
+    }
+  }, [])
+
+  return (
+    <section
+      id="features"
+      aria-label="Features for running your books"
+      className="relative overflow-hidden bg-green-600 pb-28 pt-20 sm:py-32"
+    >
+      <Container className="relative">
+        <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
+          <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
+            Maximize Your Legacy Planning
+          </h2>
+          <p className="mt-6 text-lg tracking-tight text-green-100">
+            Inaugurated just two short years ago, <strong>JOSMAXIM</strong> has
+            rapidly surged to the forefront of the investment landscape, with a
+            trail of resounding successes in its wake. Our founding team of
+            visionary financial experts, armed with cutting-edge technology and
+            a commitment to excellence, has redefined modern investing. By
+            shattering conventional norms and championing a digital-first,
+            client-centric approach, we've attracted a growing base of astute
+            investors and achieved industry recognition, propelling us onto the
+            global stage. As we celebrate our second anniversary,{' '}
+            <strong>JOSMAXIM</strong> continues to be a beacon of innovation,
+            promising a future where investors can dream bigger, aim higher, and
+            reach further.
+          </p>
+        </div>
+        <Tab.Group
+          as="div"
+          className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0"
+          vertical={tabOrientation === 'vertical'}
+        >
+          {({ selectedIndex }) => (
+            <>
+              <div className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
+                <Tab.List className="relative z-10 flex gap-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:gap-x-0 lg:gap-y-1 lg:whitespace-normal">
+                  {features.map((feature, featureIndex) => (
+                    <div
+                      key={feature.title}
+                      className={clsx(
+                        'group relative rounded-full px-4 py-1 lg:rounded-l-xl lg:rounded-r-none lg:p-6',
+                        selectedIndex === featureIndex
+                          ? 'bg-white lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10'
+                          : 'hover:bg-white/10 lg:hover:bg-white/5',
+                      )}
+                    >
+                      <h3>
+                        <Tab
+                          className={clsx(
+                            'font-display text-lg ui-not-focus-visible:outline-none',
+                            selectedIndex === featureIndex
+                              ? 'text-green-600 lg:text-white'
+                              : 'text-green-100 hover:text-white lg:text-white',
+                          )}
+                        >
+                          <span className="absolute inset-0 rounded-full lg:rounded-l-xl lg:rounded-r-none" />
+                          {feature.title}
+                        </Tab>
+                      </h3>
+                      <p
+                        className={clsx(
+                          'mt-2 hidden text-sm lg:block',
+                          selectedIndex === featureIndex
+                            ? 'text-white'
+                            : 'text-green-100 group-hover:text-white',
+                        )}
+                      >
+                        {feature.description}
+                      </p>
+                    </div>
+                  ))}
+                </Tab.List>
+              </div>
+              <Tab.Panels className="lg:col-span-7">
+                {features.map((feature) => (
+                  <Tab.Panel key={feature.title} unmount={false}>
+                    <div className="relative sm:px-6 lg:hidden">
+                      <div className="absolute -inset-x-4 bottom-[-4.25rem] top-[-6.5rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
+                      <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
+                        {feature.description}
+                      </p>
+                    </div>
+                    <div className="mt-10 w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-green-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
+                      <Image
+                        className="w-full"
+                        src={feature.image}
+                        alt=""
+                        priority
+                        sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
+                      />
+                    </div>
+                  </Tab.Panel>
+                ))}
+              </Tab.Panels>
+            </>
+          )}
+        </Tab.Group>
+      </Container>
+    </section>
+  )
+}
