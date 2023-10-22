5371f8cb23d2fc9c132402901fb296459b210743
Oscar Roblez <141037700+ruseoscar@users.noreply.github.com>
2023-10-22 09:57:32 +0800

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
+import logo from '@/images/logo.png'
 
 export function Logo(props) {
   return (
                           <Image
                         className="w-full"
-                        src={feature.image}
+                        src={logo}
                         alt=""
                         priority
                         sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
